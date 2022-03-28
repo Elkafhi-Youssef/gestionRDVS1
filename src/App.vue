59caf5dc4736f9eaef55d0dbe809835c707f55d3
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-03-28 15:06:53 +0000

    configuration tailwand

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   </nav>
+
   <router-view/>
 </template>
 
