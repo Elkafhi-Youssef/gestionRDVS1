862628a4d0a1e6cd60344b45f98781a3a2642937
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-03-31 22:43:38 +0000

    create some importante state

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,38 @@
 import { createStore } from 'vuex'
+import { createRouter } from 'vue-router';
 
 export default createStore({
   state: {
+    email:'',
+    password:'',
+    name:'youssef',
+      posts: ['post 1', 'post 2', 'post 3', 'post 4'],
+      curentRouter : 'user',
   },
-  getters: {
+  
+  getters:{
+ 
+   
+    // arrow function
+    postsCount: state => {
+      
+      return state.posts.length
+    },
+    // can access other getters
+    postsCountMessage: (state, getters) => `${getters.postsCount} posts available`
   },
+
   mutations: {
+    INSERT_POST(state,  payload){
+      state.curentRouter =payload
+      console.log(state.curentRouter);
+},
   },
-  actions: {
+  actions:{
+    async insertPost({commit}, payload){
+       //make some kind of ajax request 
+        commit('INSERT_POST', payload)
+    }
   },
   modules: {
   }
