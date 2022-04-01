c896043d20e17dfb7632c56e62c294ee008c0fec
ayoub benouahi <abenouahi5@gmail.com>
2022-04-01 15:18:48 +0000

    creat part users

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,50 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    </div>
+   
+    
+    <vs-navbar>
+      <div>
+        <vs-navbar-title>
+          RDVS
+        </vs-navbar-title>
+      </div>
+
+      <vs-navbar-item index="0" >
+        <router-link to="/">Home</router-link>
+      </vs-navbar-item>
+      <vs-navbar-item index="2">
+        <router-link to="/about">Admin</router-link>
+  
+      </vs-navbar-item>
+
+     
+    </vs-navbar>
+  </div>
+    
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  
+   data: ()=>({
+    colorx:'#1db952',
+    indexActive: 0
+  })
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+vs-navbar{
+  display: flex;
+  flex-direction: row;
+  background-color: rgb(0, 0, 0);
+  justify-content: space-between;
+  color: white;
+  padding: 1%;
 
+
+}
+
+ 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,59 @@
 <template>
+ 
   <div class="home">
-    
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+        <div>
+            <p>Nom: </p>
+            <input v-model="text1" placeholder="Nom" />
+           
+            <p>Age:</p>
+            <input v-model="number" maxlength="2" placeholder="Age" />
+            <p>Type de travail:</p>
+            <input v-model="text3" placeholder="Type de travail" />
+            
+            <p>Sex: </p>
+            <select v-model="selected">
+              <option disabled value="">Please select one</option>
+              <option>Famme</option>
+              <option>Homme</option>
+             </select>
+              
+        </div>
+        <div>
+           <p>CIN: </p>
+            <input v-model="text4" placeholder="CIN" />
+             <p>NUM de Telephone: </p>
+            <input v-model="text1" placeholder="NUM de tele" />
+            <p>Date de rendiveau: </p>
+            <input type="date" v-model="date" placeholder="NUM de tele" />
+            <p>heure exacte: </p>
+             <select v-model="selected1">
+              <option disabled value="">Please select one</option>
+              <option> 10 h à 10:30h</option>
+              <option> 11 h à 11:30h</option>
+               <option> 14 h à 14:30h</option>
+              <option> 15 h à 15:30h</option>
+               <option> 16 h à 16:30h</option>
+              
+             </select>
+             <br>
+             <button id="but">Suivant</button>
+           
+
+
+
+
+        </div>
+        
+ </div>
+ <div class="result">
+          <p>Nom: {{text1}}</p>
+          <p>Age:{{number}}</p>
+          <p>Type de travail: {{text3}}</p>
+          <p>Sex: {{selected}}</p>
+        </div>
+ 
+
+
 </template>
 
 <script>
@@ -13,6 +64,16 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  }, 
+  
+   data() {
+    return {
+      selected: [],
+      text1: [],
+       text2: [],
+        text3: [],
+        number: []
+    }
   }
 }
 </script>
